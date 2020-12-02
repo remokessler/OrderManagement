bc418f4e0eaff4dd04e544f030c024a23ac8f4e0
Remo Kessler <remo.kessler@gmx.ch>
2020-12-02 18:14:24 +0100

    DataGrid

--- OrderManagement/OrderManagement.Client/MainWindow.xaml.cs
+++ OrderManagement/OrderManagement.Client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +18,53 @@ namespace OrderManagement.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<TestObject> GridList = new ObservableCollection<TestObject>();
         public MainWindow()
         {
             InitializeComponent();
+            GridList.Add(new TestObject("Ebneter", "Marco", "Neumets nebedusse"));
+            GridList.Add(new TestObject("Kessler", "Remo", "Neumet anders nebedusse"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GridList.Add(new TestObject("Muster", "Ueli", "Save en zürcher"));
+            GenericGrid.DataContext = GridList;
+        }
+
+        public class TestObject
+        {
+            public string Name { get; set; }
+            public string Firstname { get; set; }
+            public string Adress { get; set; }
+
+            public TestObject(string name, string firstname, string adress)
+            {
+                Name = name;
+                Firstname = firstname;
+                Adress = adress;
+            }
         }
     }
 }
