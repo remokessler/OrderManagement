534c4545e61d51f0fc5d386946f411d04d5aa2e7
marco <marco.ebneter@gmail.com>
2021-01-18 18:52:46 +0100

    tests included from Remo

---
+++ OrderManagement/OrderManagement.Client/AddAddress.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OrderManagement.Client
+{
+    /// <summary> 
+    /// Interaction logic for AddAddress.xaml
+    /// </summary>
+    public partial class AddAddress : Window
+    {
+        public AddAddress()
+        {
+            InitializeComponent();
+        }
+    }
+}
