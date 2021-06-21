aa80ee44ed3f1de483065f04abda228c8bc4d973
remokessler <remo.kessler@gmx.ch>
2021-06-21 18:00:12 +0200

    added regexes

--- OrderManagement/OrderManagement.Backend/DataModels/Customer.cs
+++ OrderManagement/OrderManagement.Backend/DataModels/Customer.cs
@@ -6,10 +6,20 @@ namespace OrderManagement.Backend.DataModels
 {
     public class Customer : IHasId
     {
+        [RegularExpression(@"^CU(\d)*5$")]
         [Key]
-        public int Id { get; set; }
+        public string Id { get; set; }
         [Required]
         public string Name { get; set; }
+        // Regex from: http://emailregex.com/ as approximation to the standard
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01 -\x08\x0b\x0c\x0e -\x1f\x21\x23 -\x5b\x5d -\x7f] |\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")]
+        public string Mail { get; set; }
+        // all in example
+        [RegularExpression(@"^(http(s|):\/\/)?(w{3})?(\w*\.)+\w{2,}[\w\/\?\=\&\%]*$")]
+        public string Webpage { get; set; }
+        // min. 8 characters, small&large letter, number
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
+        public string Password { get; set; }
         [Required]
         public string Firstname { get; set; }
         [ForeignKey(nameof(Address))]
--- OrderManagement/OrderManagement.Backend/DataModels/IHasId.cs
+++ OrderManagement/OrderManagement.Backend/DataModels/IHasId.cs
@@ -2,6 +2,6 @@
 {
     public interface IHasId
     {
-        int Id { get; set; }
+        string Id { get; set; }
     }
 }
