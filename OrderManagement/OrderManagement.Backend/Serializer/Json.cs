fbd497bb14c038e51cd975e98b045d2aaf98ffe4
marco <marco.ebneter@gmail.com>
2021-09-06 18:30:28 +0200

    serializer verbessert

--- OrderManagement/OrderManagement.Backend/Serializer/Json.cs
+++ OrderManagement/OrderManagement.Backend/Serializer/Json.cs
@@ -1,17 +1,18 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Text.Json;
 using AutoMapper;
-using Newtonsoft.Json;
+using OrderManagement.Backend.DataModels;
 using OrderManagement.Backend.Repositories;
 using OrderManagement.Backend.Helpers;
+using OrderManagement.Backend.Serializer.Converter;
 using OrderManagement.Backend.Serializer.DTOs;
-using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace OrderManagement.Backend.Serializer
 {
     public class Json<T>
     {
-        private string json { get; set; }
+        private readonly string json;
 
         public Json(IRepository<T> repository)
         {
@@ -20,15 +21,17 @@ namespace OrderManagement.Backend.Serializer
                 cfg.AddProfile(new CustomerMapper());
             }));
 
-            var serializer = JsonSerializer.Create();
             var options = new JsonSerializerOptions()
             {
                 WriteIndented = true,
+                Converters = { new CustomerConverter() }
             };
 
             var entities = repository.Get().Select(entity => mapper.Map<CustomerDTO>(entity)).ToList();
 
-            var json = JsonConvert.SerializeObject(entities);
+            json = JsonSerializer.Serialize(entities, options);
+
+            //json = JsonConvert.SerializeObject(entities);
             //if (list.GetType() == typeof(Customer))
             //{
             //    foreach(var entity in dbContext.Customers)
